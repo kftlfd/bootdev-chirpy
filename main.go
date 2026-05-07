ad2dfe3b13889c55091e26192eb0536e09a9b4ca
kftlfd <kftlfd@ex.ua>
2026-05-07 14:11:42 +0300

    add health endpoint, move root to app

--- main.go
+++ main.go
@@ -8,7 +8,16 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 
 	server := http.Server{Handler: mux, Addr: ":8080"}
 	log.Fatal(server.ListenAndServe())
